944947a079b7bbbfefa365fc8994907701587c85
arconyx <1ryanc100@gmail.com>
2018-07-31 20:37:02 +1200

    Logic adapter changes
    Enable time and mathematical adapters, switch BestMatch from first response to most frequent.

--- chatterbot/chatterbot.py
+++ chatterbot/chatterbot.py
@@ -8,18 +8,25 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+
 class Chatterbot:
     def __init__(self, bot):
         self.bot = bot
         self.settings = dataIO.load_json('data/chatterbot/settings.json')
         self.chatterbot = ChatBot('Athena',
-                      database='LanLogFour',
-                      storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
-                      input_adapter="chatterbot.input.VariableInputTypeAdapter",
-                      output_adapter="chatterbot.output.OutputAdapter",
-                      output_format="text",
-                      logic_adapter=['chatterbot.logic.BestMatch']
-                      )
+                                  database='LanLogFour',
+                                  storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+                                  input_adapter="chatterbot.input.VariableInputTypeAdapter",
+                                  output_adapter="chatterbot.output.OutputAdapter",
+                                  output_format="text",
+                                  logic_adapter=[
+                                      {'import_path': 'chatterbot.logic.BestMatch',
+                                       'statement_comparison_function': 'chatterbot.comparisons.levenshtein_distance',
+                                       'response_selection_method': 'chatterbot.response_selection.get_most_frequent_response'},
+                                      'chatterbot.logic.TimeLogicAdapter',
+                                      'chatterbot.logic.MathematicalEvaluation'
+                                      ]
+                                  )
         self.conversations = {}
         self.previous_statement = {}
 
@@ -36,13 +43,13 @@ class Chatterbot:
         await self.bot.say(response)
 
     @commands.command()
-    @checks.is_owner()
+    @checks.serverowner_or_permissions(manage_server=True)
     async def chattertraindocs(self):
         """Runs training on the docs"""
         self.chatterbot.set_trainer(ListTrainer)
         with open('data/chatterbot/doc1.txt') as f:
             doc1 = f.readlines()
-        with open ('data/chatterbot/doc2.txt') as f:
+        with open('data/chatterbot/doc2.txt') as f:
             doc2 = f.readlines()
         docData = doc1 + doc2
         docData = [x.strip(',') for x in docData]
@@ -52,7 +59,7 @@ class Chatterbot:
         await self.bot.say("Training complete.")
 
     @commands.command()
-    @checks.is_owner()
+    @checks.serverowner_or_permissions(manage_server=True)
     async def chattertrain(self):
         self.chatterbot.set_trainer(ChatterBotCorpusTrainer)
         self.chatterbot.train("chatterbot.corpus.english")
@@ -60,7 +67,7 @@ class Chatterbot:
 
     @commands.command(no_pm=True, pass_context=True)
     @checks.serverowner_or_permissions(manage_server=True)
-    async def chatchannel(self, context, channel: discord.Channel=None):
+    async def chatchannel(self, context, channel: discord.Channel = None):
         """
         Set the channel to which the bot will sent its continues updates.
         Example: [p]chatchannel #talk
@@ -71,17 +78,14 @@ class Chatterbot:
             message = 'Channel set to {}'.format(channel.mention)
         elif not self.settings['CHANNEL_ID']:
             message = 'No Channel set'
-            await send_cmd_help(context)
         else:
             channel = discord.utils.get(
                 self.bot.get_all_channels(), id=self.settings['CHANNEL_ID'])
             if channel:
                 message = 'Current channel is {}'.format(channel.mention)
-                await send_cmd_help(context)
             else:
                 self.settings['CHANNEL_ID'] = None
                 message = 'No channel set'
-                await send_cmd_help(context)
 
         await self.bot.say(message)
 
@@ -90,9 +94,9 @@ class Chatterbot:
             pass
         elif message.content == '':
             pass
-        elif message.content[0] == '!': # Kludge
+        elif message.content[0] == '!':  # Kludge
             pass
-        elif message.mention_everyone == True or message.mentions != [] or message.role_mentions != []:
+        elif message.mention_everyone is True or message.mentions != [] or message.role_mentions != []:
             pass
         elif message.channel.id == self.settings['CHANNEL_ID']:
             try:
@@ -104,7 +108,7 @@ class Chatterbot:
             await self.bot.send_message(message.channel, response)
         else:
             pass
-    
+
     async def _get_response(self, input_item, conversation_id):
         input_statement = self.chatterbot.input.process_input_statement(input_item)
         # Preprocess the input statement
@@ -123,6 +127,7 @@ class Chatterbot:
         # Process the response output with the output adapter
         return str(response)
 
+
 def setup(bot):
     check_folder()
     check_file()
@@ -130,10 +135,12 @@ def setup(bot):
     bot.add_listener(chatty.listener, "on_message")
     bot.add_cog(chatty)
 
+
 def check_folder():
     if not os.path.exists('data/chatterbot'):
         print('Creating data/chatterbot folder...')
         os.makedirs('data/chatterbot')
+
 
 def check_file():
     data = {}
