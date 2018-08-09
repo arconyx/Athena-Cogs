71d6fc4a6fc5e014f710b980bf495a1e29342461
arconyx <1ryanc100@gmail.com>
2018-08-09 21:33:46 +1200

    Get text from response
    It helps to test before you push to master...

--- inspire/inspire.py
+++ inspire/inspire.py
@@ -11,7 +11,7 @@ class Inspire:
     @commands.command()
     async def inspire(self):
         async with aiohttp.get('http://inspirobot.me/api?generate=true') as quote:
-            await self.bot.say(quote)
+            await self.bot.say(quote.text())
 
 
 def setup(bot):
