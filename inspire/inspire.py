dd46eefdb6214ea7ef1b2099aea63068413078d8
arconyx <1ryanc100@gmail.com>
2018-08-09 21:40:31 +1200

    Await the async function
    When working with async based libraries, await is helpful.

--- inspire/inspire.py
+++ inspire/inspire.py
@@ -11,7 +11,7 @@ class Inspire:
     @commands.command()
     async def inspire(self):
         async with aiohttp.get('http://inspirobot.me/api?generate=true') as quote:
-            await self.bot.say(quote.text())
+            await self.bot.say(await quote.text())
 
 
 def setup(bot):
